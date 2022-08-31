c458176677dd6bfd4b0aab11cb246494beb63ab7
Rohan Akki <roboy13@yahoo.com>
2022-08-31 15:12:55 -0400

    completed server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+const http = require('http');
+
+const fs = require('fs');
+
+const minimist = require('minimist');
+
+var args = minimist(process.argv.slice(2));
+
+// Use minimist to process one argument `--port=` on the command line after `node server.js`.
+
+const port = args.port || process.env.port || 3000;
+
+fs.readFile('./public/index.html', 'utf8', (err, html) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+
+    const server = http.createServer((req, res) => {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/html')
+        res.end('./public/index.html')
+    });
+
+
+    server.listen(port, () => {
+        console.log(`Server listening on port ${port}`)
+    });
+
+});
+
+// That's it! You're all done!
